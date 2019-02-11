8f0d7fc690c0cccaaf6668b4c90c55fad28e0b36
Martin <martin.mihalik.nr@gmail.com>
2019-02-11 11:32:12 +0100

    First message

---
+++ app.js
@@ -0,0 +1,1 @@
+var taxi = $('#taxi');
---
+++ logic.js
@@ -0,0 +1,19 @@
+var myTaxi = {
+    name: 'Crazy Taxi'
+}
+
+function Game(obstacles, taxi) {
+    this.cars = obstacles; // Array of objects
+    this.taxi = taxi;   // Object
+}
+
+function Obstacles(name, positionX, positionY, urlImage) {
+    this.name = name;
+    this.positionX = positionX;
+    this.positionY = positionY;
+    this.image = urlImage;
+}
+
+switch(event) {
+    
+}
